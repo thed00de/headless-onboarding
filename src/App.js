9d50ae0823046505e4bad39841fe1b9e59520cf1
MustyMouse <admin@krux.us>
2020-09-22 21:32:27 -0500

    Spelling fix + hoping 404s have stopped

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  My Secoond Deployed App on WP Engine's Headless Platform!
+	  My Second Deployed App on WP Engine's Headless Platform!
         </p>
       </header>
     </div>
